7b49e31926303c828092e6828ecc2498f9aecd49
kynm <kynm@hus.edu.vn>
2020-05-23 15:15:10 +0700

    update code

--- js/baoduongcanhan.js
+++ js/baoduongcanhan.js
@@ -1,4 +1,3 @@
-
     function readFile() {
       if (this.files && this.files[0]) {
         var FR= new FileReader();
@@ -6,22 +5,60 @@
         document.getElementById("img").src = e.target.result;
         var width = 500;
         var height = 300;
-        var imageconvert = new Image();
-        imageconvert.src = e.target.result;
-        imageconvert.onload = () => {
-                const elem = document.createElement('canvas');
-                elem.width = width;
-                elem.height = height;
-                const ctx = elem.getContext('2d');
-                // img.width and img.height will contain the original dimensions
-                ctx.drawImage(imageconvert, 0, 0, width, height);
+        var image = new Image();
+        image.src = e.target.result;
+var dataURLToBlob = function(dataURL) {
+    var BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+        var parts = dataURL.split(',');
+        var contentType = parts[0].split(':')[1];
+        var raw = parts[1];
+
+        return new Blob([raw], {type: contentType});
+    }
+
+    var parts = dataURL.split(BASE64_MARKER);
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+
+    var uInt8Array = new Uint8Array(rawLength);
+
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], {type: contentType});
+}
+        image.onload = () => {
+                // Resize the image
+                var canvas = document.createElement('canvas'),
+                    max_size = 800,// TODO : pull max size from a site config
+                    width = image.width,
+                    height = image.height;
+                if (width > height) {
+                    if (width > max_size) {
+                        height *= max_size / width;
+                        width = max_size;
+                    }
+                } else {
+                    if (height > max_size) {
+                        width *= max_size / height;
+                        height = max_size;
+                    }
+                }
+                canvas.width = width;
+                canvas.height = height;
+                canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+                var dataUrl = canvas.toDataURL('image/jpeg');
+                var resizedImage = dataURLToBlob(dataUrl);
                 var ID_DOTBD = $("#ID_DOTBD").val();
                 var urluploadimage = $("#urluploadimage").val();
                 $.ajax({
                     url: urluploadimage,
                     method: 'post',
                     data: {
-                        IMAGEBASE64: imageconvert.src,
+                        IMAGEBASE64: dataUrl,
                         ID_DOTBD: ID_DOTBD,
                     },
                     success:function(data) {
