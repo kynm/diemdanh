4b26470294793bb0d887d1de8b7d9fea6d0fee38
kynm <kynm@hus.edu.vn>
2020-06-11 17:12:24 +0700

    update code

--- views/quanlymayno/inbaoduongthang.php
+++ views/quanlymayno/inbaoduongthang.php
@@ -26,19 +26,25 @@
     'dongiamayno' => $dongiamayno,
 ]) ?>
 <table class="table">
-    <tr style="border: none;height: 100px;">
+    <tr style="border: none;height: 20px;">
         <td colspan="2" style="text-align: left;border: none;font-weight: bold;">Nơi gửi</td>
-        <td colspan="4"style="text-align: center;border: none;font-weight: bold;">Người lập biểu</td>
+        <td colspan="4"style="text-align: center;border: none;font-weight: bold;width: 50%;">Người lập biểu</td>
         <td colspan="2" style="text-align: center;border: none;font-weight: bold;;">TRUNG TÂM ĐIỀU HÀNH THÔNG TIN</td>
     </tr>
-    <tr></tr>
-    <tr></tr>
-    <tr></tr>
-    <tr></tr>
-    <tr></tr>
+    <tr>
+        <td colspan="2" style="border: none">
+            Nơi gửi
+            <br/>KTTKTC (Thực hiện)
+            <br/>Lưu: TTĐHTT
+        </td>
+        <td colspan="4" style="border: none"></td>
+        <td colspan="2" style="border: none"></td>
+    </tr>
+    <tr>
+    </tr>
     <tr style="border: none;">
-        <td colspan="2" style="text-align: left;border: none;">Giám đốc, Phó giám đốc(thay báo cáo)</td>
-        <td colspan="4"style="text-align: center;border: none;"><?php echo Yii::$app->user->identity->nhanvien->TEN_NHANVIEN;?></td>
+        <td colspan="2" style="text-align: left;border: none;"></td>
+        <td colspan="4"style="text-align: center;border: none;width: 50%;"><?php echo Yii::$app->user->identity->nhanvien->TEN_NHANVIEN;?></td>
         <td colspan="2" style="text-align: center;border: none;"></td>
     </tr>
 </table>
