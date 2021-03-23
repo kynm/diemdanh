452a54d2d2365efb1103a0ab7f897cc337653e9d
kynm <kynm@hus.edu.vn>
2021-03-24 00:39:09 +0700

    update code

--- views/layouts/header_ioc.php
+++ views/layouts/header_ioc.php
@@ -6,13 +6,18 @@ use yii\helpers\Url;
     <?= Html::csrfMetaTags() ?>
     <nav class="navbar navbar-static-top" role="navigation">
       <nav class="">
+          <div class="">
+              <ul class="nav navbar-nav">
+                <li><a href="<?= Url::to(['ioc/phanbothietbi'])?>">Bản đồ phân bố thiết bị</a></li>
+                <li><a href="<?= Url::to(['ioc/phanbospliter'])?>">Phân bố spliter</a></li>
+                <li><a href="<?= Url::to(['ioc/phanbothuebao'])?>">Phân bố thuê bao</a></li>
+                <li><a href="<?= Url::to(['ioc/baocaothiphanthuebao'])?>">Thị phần thuê bao</a></li>
+                <li><a href="<?= Url::to(['ioc/baocaothiphanthuebao'])?>">Điều hành cập nhật tọa độ thuê bao</a></li>
+              </ul>
+          </div>
           <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
-              <li><a href="<?= Url::to(['ioc/phanbothietbi'])?>">Bản đồ phân bố thiết bị</a></li>
-              <li><a href="<?= Url::to(['ioc/phanbospliter'])?>">Phân bố spliter</a></li>
-              <li><a href="<?= Url::to(['ioc/phanbothuebao'])?>">Phân bố thuê bao</a></li>
-              <li><a href="<?= Url::to(['ioc/baocaothiphanthuebao'])?>">Báo cáo thị phần thuê bao</a></li>
-              <li><a href="<?= Url::to(['site/ioc'])?>">IOC</a></li>
+              <li><a href="<?= Url::to(['/'])?>">Trang chủ</a></li>
               <li class="task-menu">
                 <a href="<?= Url::to(['site/logout'])?>" data-method="post">
                   <i class="fa fa-sign-out"></i> Đăng xuất
