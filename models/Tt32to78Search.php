514317809de20cb53d2c9d7a1abc67ccaf2d30fa
kynm <kynm@hus.edu.vn>
2022-04-12 22:47:11 +0700

    update code

--- models/Tt32to78Search.php
+++ models/Tt32to78Search.php
@@ -112,4 +112,38 @@ class Tt32to78Search extends Tt32to78
         return $dataProvider;
     }
 
+    public function searchtrangchu($params)
+    {
+        $query = Tt32to78::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+        $query->joinWith('nhanvien');
+
+        // $query->andFilterWhere(['not in', 'ketqua', [4,5,6,7,8,9]]);
+        $query->andFilterWhere(['like', 'MST', $this->MST]);
+        $query->andFilterWhere(['like', 'TEN_KH', $this->TEN_KH]);
+        $query->andFilterWhere(['like', 'DIACHI', $this->DIACHI]);
+        $query->andFilterWhere(['like', 'LIENHE', $this->LIENHE]);
+        $query->andFilterWhere(['like', 'EMAIL', $this->EMAIL]);
+        $query->andFilterWhere(['like', 'nhanvien.TEN_NHANVIEN', $this->nhanvien_id]);
+        $query->andFilterWhere(['=', 'TRANGTHAINANGCAP', $this->TRANGTHAINANGCAP]);
+        $query->orderBy([
+            'ngay_lh' => SORT_DESC,
+            'ketqua' => SORT_DESC,
+        ]);
+        return $dataProvider;
+    }
+
 }
--- views/site/index.php
+++ views/site/index.php
@@ -1,136 +1,25 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
-use yii\widgets\ActiveForm;
-use yii\widgets\Pjax;
-use kartik\select2\Select2;
 use yii\helpers\Url;
 use yii\helpers\ArrayHelper;
-use dosamigos\chartjs\ChartJs;
-use app\models\ActivitiesLog;
-use app\models\Baoduongtong;
+use kartik\grid\GridView;
+use yii\widgets\Pjax;
 use app\models\Daivt;
-use app\models\Donvi;
-use app\models\Dotbaoduong;
-use app\models\Images;
-use app\models\Nhomtbi;
-use app\models\Noidungcongviec;
 use app\models\Tramvt;
-$dsDonvi = ArrayHelper::map(Donvi::find()->where(['in', 'ID_DONVI', [2,3,4,5,6,7]])->all(), 'ID_DONVI', 'TEN_DONVI');
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
-/* @var $searchModel app\models\DotbaoduongSearch */
+/* @var $searchModel app\models\TramvtSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'RA QUÂN - ' . $text;
+$this->title = 'Điều hành chiến dịch HDDT mới';
+$this->params['breadcrumbs'][] = $this->title;
+
 ?>
-<div class="index">
-    <div class="row">
-        <div class="row">
-            <?= $this->render('/partial/_link_search_with_date_type', [
-                'url' => '/',
-                'type' => $type,
-            ]) ?>
-            <div class="col-md-4">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3><b>TỔNG HỢP NÂNG CẤP HỆ THỐNG</b></h3>
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-bordered">
-                            <tbody>
-                                <tr class="bg-primary">
-                                    <th style="width: 10px">#</th>
-                                    <th>TÊN NHÂN VIÊN</th>
-                                    <th>ĐÃ NÂNG CẤP</th>
-                                    <th>CÒN LẠI</th>
-                                    <th>TỔNG SỐ</th>
-                                    <th>TỶ LỆ</th>
-                                </tr>
-                                <?php foreach ($dsnangcaphethong as $key => $value):?>
-                                    <tr>
-                                        <td scope="col"><?php echo ($key + 1)?></td>
-                                        <td scope="col"><?php echo $value['TEN_NHANVIEN']?></td>
-                                        <td scope="col"><?php echo $value['DANANGCAP']?></td>
-                                        <td scope="col"><?php echo ($value['TONGSO']  - $value['DANANGCAP'])?></td>
-                                        <td scope="col"><?php echo $value['TONGSO']?></td>
-                                        <td scope="col"><?php echo round(($value['DANANGCAP'] * 100)/$value['TONGSO'], 2)?> %</td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3><b>TỔNG HỢP ĐÃ HOÀN THÀNH</b></h3>
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-bordered">
-                            <tbody>
-                                <tr class="bg-primary">
-                                    <th style="width: 10px">#</th>
-                                    <th>TÊN NHÂN VIÊN</th>
-                                    <th>ĐÃ HOÀN THÀNH</th>
-                                    <th>CÒN LẠI</th>
-                                    <th>TỔNG SỐ</th>
-                                    <th>TỶ LỆ</th>
-                                </tr>
-                                <?php foreach ($dahoanthanh as $key => $value):?>
-                                    <tr>
-                                        <td scope="col"><?php echo ($key + 1)?></td>
-                                        <td scope="col"><?php echo $value['TEN_NHANVIEN']?></td>
-                                        <td scope="col"><?php echo $value['DAHOANTHANH']?></td>
-                                        <td scope="col"><?php echo ($value['TONGSO']  - $value['DAHOANTHANH'])?></td>
-                                        <td scope="col"><?php echo $value['TONGSO']?></td>
-                                        <td scope="col"><?php echo round(($value['DAHOANTHANH'] * 100)/$value['TONGSO'], 2)?> %</td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3><b>KẾT QUẢ CHUYỂN ĐỔI HÓA ĐƠN SANG TT 78(THEO LƯỢT HỖ TRỢ)</b></h3>
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-bordered">
-                            <tbody>
-                                <tr class="bg-primary">
-                                    <th style="width: 10px">#</th>
-                                    <th>TÊN NHÂN VIÊN</th>
-                                    <th>ĐÃ LH, HẸN LÚC KHÁC</th>
-                                    <th>ĐÃ THÊM VÀO NHÓM ZALO</th>
-                                    <th>ĐÃ TƯ VẤN NGHIỆP VỤ</th>
-                                    <th>ĐÃ GỬI DK01</th>
-                                    <th>ĐÃ PHÁT HÀNH HĐ</th>
-                                    <th>ĐÃ HD NGHIỆP VỤ SAU PHÁT HÀNH</th>
-                                    <th>ĐÃ HỦY</th>
-                                </tr>
-                                <?php foreach ($dsketquachuyendoi as $key => $value):?>
-                                    <tr>
-                                        <td scope="col"><?php echo ($key + 1)?></td>
-                                        <td scope="col"><?php echo $value['TEN_NHANVIEN']?>
-                                        <td scope="col"><?php echo $value['DALH']?>
-                                        <td scope="col"><?php echo $value['DATHEMZALO']?>
-                                        <td scope="col"><?php echo $value['GUIDK01']?>
-                                        <td scope="col"><?php echo $value['DAPHHD']?>
-                                        <td scope="col"><?php echo $value['HDNVKHAC']?>
-                                        <td scope="col"><?php echo $value['DADUNGDNK']?>
-                                        <td scope="col"><?php echo $value['HUYDV']?>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+<div class="tramvt-index">
+
+    <div class="box box-primary">
             <div class="col-md-12">
                 <div class="box">
                     <div class="box-header with-border">
@@ -142,127 +31,84 @@ $this->title = 'RA QUÂN - ' . $text;
                                 <tr class="bg-primary">
                                     <th style="width: 10px">#</th>
                                     <th>TÊN NHÂN VIÊN</th>
-                                    <th>ĐÃ LH, HẸN LÚC KHÁC</th>
-                                    <th>ĐÃ THÊM VÀO NHÓM ZALO</th>
-                                    <th>ĐÃ TƯ VẤN NGHIỆP VỤ</th>
-                                    <th>ĐÃ GỬI DK01</th>
-                                    <th>ĐÃ PHÁT HÀNH HĐ</th>
-                                    <th>ĐÃ HD NGHIỆP VỤ SAU PHÁT HÀNH</th>
+                                    <th>ĐÃ LH</th>
+                                    <th>ĐÃ GỬI DK01 - HOÀN THÀNH NÂNG CẤP</th>
+                                    <th>ĐÃ DÙNG DOANH NGHIỆP KHÁC</th>
                                     <th>ĐÃ HỦY</th>
-                                </tr>
-                                <?php foreach ($dsketquachuyendoitheocongty as $key => $value):?>
-                                    <tr>
-                                        <td scope="col"><?php echo ($key + 1)?></td>
-                                        <td scope="col"><?php echo $value['TEN_NHANVIEN']?>
-                                        <td scope="col"><?php echo $value['DALH']?>
-                                        <td scope="col"><?php echo $value['DATHEMZALO']?>
-                                        <td scope="col"><?php echo $value['GUIDK01']?>
-                                        <td scope="col"><?php echo $value['DAPHHD']?>
-                                        <td scope="col"><?php echo $value['HDNVKHAC']?>
-                                        <td scope="col"><?php echo $value['DADUNGDNK']?>
-                                        <td scope="col"><?php echo $value['HUYDV']?>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <?php if(0) :?>
-            <div class="col-md-6">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3><b>KHÁCH HÀNG ĐÃ TIẾP CẬN THEO ĐỊA BÀN</b></h3>
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-bordered">
-                            <tbody>
-                                <tr class="bg-primary">
-                                    <th style="width: 10px">#</th>
-                                    <th>Đơn vị</th>
-                                    <th>Số lượng</th>
-                                </tr>
-                                <?php foreach ($tongkhdatiepcan as $key => $value):?>
-                                    <tr>
-                                        <td scope="col"><?php echo ($key + 1)?></td>
-                                        <td scope="col"><?php echo $value['TEN_DONVI']?>
-                                        <td scope="col"><?php echo $value['SO_LUONG']?>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3><b>HÌNH THỨC TIẾP CẬN</b></h3>
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-bordered">
-                            <tbody>
-                                <tr class="bg-primary">
-                                    <th style="width: 10px">#</th>
-                                    <th>ĐỊA BÀN</th>
-                                    <th>GỌI ĐIỆN</th>
-                                    <th>GẶP TRỰC TIẾP</th>
-                                </tr>
-                                <?php foreach ($cachtiepcan as $key => $value):?>
-                                    <tr>
-                                        <td scope="col"><?php echo ($key + 1)?></td>
-                                        <td scope="col"><?php echo $value['TEN_DONVI']?>
-                                        <td scope="col"><?php echo $value['GOIDIEN']?>
-                                        <td scope="col"><?php echo $value['GAPTRUCTIEP']?>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="box">
-                    <div class="box-header with-border">
-                        <h3><b>KẾT QUẢ TIẾP CẬN</b></h3>
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-bordered">
-                            <tbody>
-                                <tr class="bg-primary">
-                                    <th style="width: 10px">#</th>
-                                    <th>ĐỊA BÀN</th>
-                                    <th>ĐỒNG Ý</th>
-                                    <th>KÝ HỢP ĐỒNG</th>
-                                    <th>THU TIỀN</th>
-                                    <th>ĐÃ HOÀN THIỆN</th>
-                                    <th>ĐÃ DÙNG TRƯỚC</th>
-                                    <th>ĐÃ DÙNG DN KHÁC</th>
                                     <th>GIẢI THỂ</th>
-                                    <th>SÁT NHẬP</th>
-                                    <th>HẸN GỌI SAU</th>
                                 </tr>
-                                <?php foreach ($dsketquatiepcan as $key => $value):?>
+                                <?php
+                                $tongdalienhe = 0;
+                                $tongdanangcap = 0;
+                                $tongdadungdoanhnghiepkhac = 0;
+                                $tongdahuy = 0;
+                                $tonggiaithe = 0;
+                                    ?>
+                                <?php foreach ($dsketquachuyendoitheocongty as $key => $value):?>
+                                    <?php
+                                $tongdalienhe += $value['DALH'] + $value['DATHEMZALO'] + $value['TVNV'] + $value['GUIDK01'] + $value['DAPHHD'] + $value['HDNVKHAC'] + $value['DADUNGDNK'] + $value['HUYDV'] + $value['GIAITHE'];
+                                $tongdanangcap += $value['GUIDK01'] + $value['DAPHHD'] + $value['HDNVKHAC']  + $value['DADUNGDNK'] + $value['HUYDV'] + $value['GIAITHE'];
+                                $tongdadungdoanhnghiepkhac += $value['DADUNGDNK'];
+                                $tongdahuy += $value['HUYDV'];
+                                $tonggiaithe += $value['GIAITHE'];
+                                    ?>
                                     <tr>
                                         <td scope="col"><?php echo ($key + 1)?></td>
-                                        <td scope="col"><?php echo $value['TEN_DONVI']?>
-                                        <td scope="col"><?php echo $value['DONGY']?>
-                                        <td scope="col"><?php echo $value['KYHOPDONG']?>
-                                        <td scope="col"><?php echo $value['THUTIEN']?>
-                                        <td scope="col"><?php echo $value['HOANTHIEN']?>
-                                        <td scope="col"><?php echo $value['DADUNGTRUOC']?>
-                                        <td scope="col"><?php echo $value['DADUNGDNK']?>
-                                        <td scope="col"><?php echo $value['GIAITHE']?>
-                                        <td scope="col"><?php echo $value['SATNHAP']?>
-                                        <td scope="col"><?php echo $value['HENGOISAU']?>
+                                        <td scope="col"><?php echo $value['TEN_NHANVIEN']?></td>
+                                        <td scope="col"><?php echo ($value['DALH'] + $value['DATHEMZALO'] + $value['TVNV'] + $value['GUIDK01'] + $value['DAPHHD'] + $value['HDNVKHAC'] + $value['DADUNGDNK'] + $value['HUYDV'] + $value['GIAITHE'])?></td>
+                                        <td scope="col"><?php echo ($value['GUIDK01'] + $value['DAPHHD'] + $value['HDNVKHAC']  + $value['DADUNGDNK'] + $value['HUYDV'] + $value['GIAITHE'])?></td>
+                                        <td scope="col"><?php echo $value['DADUNGDNK']?></td>
+                                        <td scope="col"><?php echo $value['HUYDV']?></td>
+                                        <td scope="col"><?php echo $value['GIAITHE']?></td>
                                     </tr>
                                 <?php endforeach; ?>
+                                <tr>
+                                        <td scope="col"><?php echo ($key + 1)?></td>
+                                        <td scope="col"></td>
+                                        <td scope="col"><?php echo $tongdalienhe?></td>
+                                        <td scope="col"><?php echo $tongdanangcap?></td>
+                                        <td scope="col"><?php echo $tongdadungdoanhnghiepkhac?></td>
+                                        <td scope="col"><?php echo $tongdahuy?></td>
+                                        <td scope="col"><?php echo $tonggiaithe?></td>
+                                    </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
-            <?php endif; ?>
+        <div class="box-body">
+            <?php Pjax::begin(); ?><?= GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'filterModel' => $searchModel,
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+                        [
+                            'attribute' => 'MST',
+                            'value' => 'MST',
+                            'contentOptions' => ['style' => 'width:10%; white-space: normal;'],
+                        ],
+                        [
+                            'attribute' => 'tt32to78_id',
+                            'value' => 'TEN_KH',
+                            'contentOptions' => ['style' => 'width:10%; white-space: normal;'],
+                        ],
+                        [
+                            'attribute' => 'nhanvien_id',
+                            'value' => 'nhanvien.TEN_NHANVIEN'
+                        ],
+                        [
+                            'attribute' => 'ghichu',
+                            'value' => function ($model) {
+                                return $model->ghichu;
+                            },
+                            // 'contentOptions' => ['style' => 'width:20%; white-space: normal;'],
+                            'format' => 'raw',
+                        ],
+                        'ngay_lh',
+                    ],
+                ]); ?>
+            <?php Pjax::end(); ?>
+            
         </div>
     </div>
 </div>
