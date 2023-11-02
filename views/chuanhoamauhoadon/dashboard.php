e6a649296e0f53fbcf9ba9c1e05ae3a02ee081ec
kynm <kynm.hnm@vnpt.vn>
2023-11-02 17:55:45 +0700

    update code

--- views/chuanhoamauhoadon/dashboard.php
+++ views/chuanhoamauhoadon/dashboard.php
@@ -7,12 +7,16 @@ use yii\widgets\Pjax;
 use kartik\select2\Select2;
 use yii\helpers\Url;
 use yii\helpers\ArrayHelper;
-
+$date0= date_create(date('2023-11-03'));
+$date1= date_create(date('Y-m-d'));
+$date2 = date_create("2023-12-03");
+$diff = date_diff($date1,$date2);
+$diff1 = date_diff($date0,$date1);
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\DotbaoduongSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'BÁO TỔNG HỢP';
+$this->title = 'THỜI GIAN THỰC HIỆN TỬ NGÀY 03/11/2023 đến 03/12/2023. THỜI GIAN CÒN LẠI: ' . $diff->days . ' NGÀY';
 ?>
 <div class="index">
     <div class="row">
@@ -26,11 +30,13 @@ $this->title = 'BÁO TỔNG HỢP';
                                 <th>NHÂN VIÊN</th>
                                 <th>SỐ LƯỢNG</th>
                                 <th>SỐ LƯỢNG CẦN THỰC HIỆN/NGÀY</th>
+                                <th>SỐ LƯỢNG CẦN THỰC HIỆN ĐẾN HÔM NAY</th>
                                 <th>CHƯA THỰC HIỆN</th>
                                 <th>ĐANG YÊU CẦU SỬA</th>
                                 <th>ĐÃ SỬA</th>
                                 <th>HOÀN THÀNH SỬA</th>
                                 <th>KHÔNG CẦN SỬA</th>
+                                <th>TỈ LỆ HOÀN THÀNH</th>
                             </tr>
                             <?php
                              foreach ($baocaotonghop as $key => $value):
@@ -40,6 +46,7 @@ $this->title = 'BÁO TỔNG HỢP';
                                     <td scope="col"><?php echo $value['TEN_NHANVIEN']?></td>
                                     <td scope="col"><?php echo $value['SO_LUONG']?></td>
                                     <td scope="col"><?php echo $value['SO_LUONG_NGAY']?></td>
+                                    <td scope="col"><?php echo $value['SO_LUONG_NGAY'] * ($diff->days + 1)?></td>
                                     <td scope="col"><?php echo $value['CHUA_TH']?></td>
                                     <td scope="col"><?php echo $value['DANG_YC_SUA']?></td>
                                     <td scope="col"><?php echo $value['CHUA_DA_SUA']?></td>
